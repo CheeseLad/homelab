62c2641fad1112a7617920e5acfd023d17dc16a7
Jake Farrell <jake_farrell@outlook.com>
2026-04-14 17:24:54 +0100

    remove old scripts, update rclone filters

--- scripts/rclone/unused/copy-mps-vps.sh
+++
@@ -1,4 +0,0 @@
-rclone move "DCU-GCrypt-2024:/DCUMPS" "/mnt/usb1/Jake/Device Automated Backups/DCU MPS VPS" -v \
-  --bwlimit 1.8M \
-  --config /home/jake/scripts/rclone/rclone.conf \
-  --log-file "/home/jake/scripts/rclone/copy-mps-vps.log"
